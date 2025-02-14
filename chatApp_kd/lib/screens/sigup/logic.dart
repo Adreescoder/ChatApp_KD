7e2fe6d8e5233b2654fe087fe898fcf983415b1c
Adrees Coder <adreesking975@gmail.com>
2025-02-14 05:40:06 -0800

    chat app ka code lakeiya ho

--- chatApp_kd/lib/screens/sigup/logic.dart
+++ chatApp_kd/lib/screens/sigup/logic.dart
@@ -1,58 +1,33 @@
-import 'dart:io';
-import 'package:chatapp_kd/modles/user.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 
-class SignupScreenLogic extends GetxController {
-  var selectedImage = Rx<File?>(null);
-  var isLoading = false.obs;
+class SigupLogic extends GetxController {
+  
+  TextEditingController nameController = TextEditingController() ;
+  TextEditingController emailController = TextEditingController() ;
+  TextEditingController passwordController = TextEditingController() ;
+  final FirebaseAuth _auth = FirebaseAuth.instance ;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance ;
 
-  TextEditingController nameController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  Future<void> signupFirebase() async {
-    if (nameController.text.isEmpty ||
-        emailController.text.isEmpty ||
-        passwordController.text.isEmpty) {
-      Get.snackbar("Error", "All fields are required!",
-          snackPosition: SnackPosition.BOTTOM);
-      return;
+  Future<void> sigupUser()async{
+    
+    String name = nameController.text ;
+    String email = nameController.text ;
+    String password = nameController.text ;
+    
+    if(name.isEmpty|| email.isEmpty||password.isEmpty){
+      Get.snackbar(
+          "Error",
+          "Please fil the all filled");
+      return ;
     }
+    else{
 
-    try {
-      isLoading.value = true;
-
-      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-        email: emailController.text.trim(),
-        password: passwordController.text.trim(),
-      );
-
-      if (userCredential.user != null) {
-      String userId =  userCredential.user!.uid ;
-        UserModel userModel = UserModel(id: userId, name: nameController.text, email: emailController.text, password: passwordController.text,  imageUrl: imageUrl, isOnline: isOnline);
-        await _firestore.collection('users').doc(userCredential.user!.uid).set({
-          'uid': userCredential.user!.uid,
-          'name': nameController.text,
-          'email': emailController.text,
-          'imageUrl': '',
-          'createdAt': Timestamp.now(),
-        });
-
-        Get.snackbar("Success", "Signup Successful!",
-            snackPosition: SnackPosition.BOTTOM);
-      }
-    } catch (e) {
-      Get.snackbar("Error", e.toString(), snackPosition: SnackPosition.BOTTOM);
-    } finally {
-      isLoading.value = false;
+     UserCredential userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password) ;
+     U
     }
   }
+  
 }
-/// ok
