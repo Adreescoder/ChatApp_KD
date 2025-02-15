346606cf261f0098d7afb53a19ee3b119a9d0958
Adrees Coder <adreesking975@gmail.com>
2025-02-14 23:32:21 -0800

    ok

--- chatApp_kd/lib/screens/login/logic.dart
+++ chatApp_kd/lib/screens/login/logic.dart
@@ -1,5 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginLogic extends GetxController {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  var isLoading = false.obs; // Observable for loading state
+
+  Future<void> loginUser() async {
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+
+    if (email.isEmpty || password.isEmpty) {
+      Get.snackbar("Error", "Please enter email and password");
+      return;
+    }
+
+    isLoading.value = true; // Show loading
+
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+      Get.snackbar("Success", "Login Successful!");
+      Get.offAllNamed("/home"); // Navigate to HomeScreen
+    } on FirebaseAuthException catch (e) {
+      Get.snackbar("Login Failed", e.message ?? "Something went wrong");
+    } finally {
+      isLoading.value = false; // Hide loading
+    }
+  }
 }
