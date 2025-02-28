9e14dfd7871ac7dd58d71f284e8e12aafd7424e0
Adrees Coder <adreesking975@gmail.com>
2025-02-28 16:24:19 +0500

    you

--- chatApp_kd/lib/screens/chat/logic.dart
+++ chatApp_kd/lib/screens/chat/logic.dart
@@ -10,5 +10,4 @@ class ChatLogic extends GetxController {
   var message = <Message>[].obs ;
   FirebaseFirestore firestore = FirebaseFirestore.instance ;
   FirebaseAuth auth = FirebaseAuth.instance ;
-// master oo
 }
